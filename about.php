1f60dbf831676dbf47634b4d5c085ddb91e90fe8
Muhammad Zaki <mahmadzekoo@gmail.com>
2024-02-11 22:16:29 +0200

    Update about.php

--- about.php
+++ about.php
@@ -221,7 +221,7 @@
 
         </div>
 
-        <div class="credit"> created by <span>Muhammad Zaki</span> | all rights reserved! </div>
+        <div class="credit">&copy; <?php echo $year = date('Y'); ?> Muhammad Zaki</div>
 
     </section>
 
